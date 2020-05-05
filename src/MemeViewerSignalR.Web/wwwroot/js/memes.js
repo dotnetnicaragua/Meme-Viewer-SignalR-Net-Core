0aeb1ce710b9db93afe50c728bf6e9ce6fc53ef8
Kevin Irías <kevin.irias@inaxir.com>
2020-05-04 20:14:26 -0600

    Added basic connect handling to signal r

---
+++ src/MemeViewerSignalR.Web/wwwroot/js/memes.js
@@ -0,0 +1,21 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder()
+    .withUrl("/memesHub")
+    .build();
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+
+
+function startConnection() {
+    connection.start().then(function () {
+        document.getElementById("sendButton").disabled = false;
+        console.log('connected');
+    }).catch(function (err) {
+        return console.error(err.toString());
+    });
+}
+
+startConnection();
