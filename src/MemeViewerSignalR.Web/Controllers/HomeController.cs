904cd64cec52ab684598e3c6a7ba5a9e15125fb6
Kevin Irías <kevin.irias@inaxir.com>
2020-05-04 22:04:39 -0600

    Added endpoints to add memes from external sources

--- src/MemeViewerSignalR.Web/Controllers/HomeController.cs
+++ src/MemeViewerSignalR.Web/Controllers/HomeController.cs
@@ -6,21 +6,40 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MemeViewerSignalR.Web.Models;
+using Microsoft.AspNetCore.SignalR;
+using MemeViewerSignalR.Web.Hubs;
 
 namespace MemeViewerSignalR.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IHubContext<MemeHub, IMemeHubClient> memeHubContext;
+        public HomeController(ILogger<HomeController> logger,
+            IHubContext<MemeHub, IMemeHubClient> memeHubContext)
         {
             _logger = logger;
+            this.memeHubContext = memeHubContext;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CustomAdd([FromBody] Meme meme)
+        {
+            await memeHubContext.Clients.All.ReceiveMeme(meme);
+            return Ok();
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> CustomAddSecret([FromBody] Meme meme)
+        {
+            await memeHubContext.Clients.Group("secret").ReceiveMeme(meme);
+            return Ok();
+
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
